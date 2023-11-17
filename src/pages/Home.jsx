a974e7470a1eec403ef97672b4e4c48f9f899df5
Teun Jansen <teunjansen91@gmail.com>
2023-11-17 20:23:54 +0100

    assets folder and basic homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,53 @@
+import { Suspense } from "react"
+import { Canvas } from "@react-three/fiber"
+import Loader from "../components/Loader"
+import Island from "../models/Island"
 
+
+{/*   <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        POPUP
+      </div>*/}
 
 const Home = () => {
+/*Function to see the screensize and the adjust the 3d model to its size */
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
-    <div>Home</div>
+    <section className="w-full h-screen relative">
+      <Canvas 
+        className="w-full h-screen bg-transparent"
+        camera={{near: 0.1, far:1000}}
+      >
+        <Suspense fallback={<Loader />}>
+          {/*Different light components to make the model look good */}
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+
+          <Island
+            position ={islandPosition}
+            scale = {islandScale}
+            rotation = {islandRotation}
+          />
+        </Suspense>  
+      </Canvas>
+    </section>
   )
 }
 export default Home
