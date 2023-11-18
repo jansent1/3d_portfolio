53080823207a39fd838f0793fc0fe7abecc52ad4
Teun Jansen <teunjansen91@gmail.com>
2023-11-18 04:52:32 +0100

    Added homepage models

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { Canvas } from "@react-three/fiber"
 import Loader from "../components/Loader"
 import Island from "../models/Island"
 import Sky from "../models/Sky"
+import Bird from "../models/Bird"
+import Plane from "../models/Plane"
 
 
 {/*   <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -43,13 +45,14 @@ const Home = () => {
           {/*Emits light from one direction in the shape of a collon with <spotLight /> */}
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
-
+          <Bird />
           <Sky />
           <Island
             position ={islandPosition}
             scale = {islandScale}
             rotation = {islandRotation}
           />
+          <Plane />
         </Suspense>  
       </Canvas>
     </section>
